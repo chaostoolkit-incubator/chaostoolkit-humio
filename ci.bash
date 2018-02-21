d7ffac6669c73a0fa000a00bdb168186b4b5ab8f
Sylvain Hellegouarch <sh@defuze.org>
2018-02-21 14:13:50 +0100

    Added travis CI build
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- ci.bash
+++ ci.bash
@@ -16,10 +16,27 @@ function run-test () {
     python setup.py test
 }
 
+function release () {
+    echo "Releasing the package"
+    python setup.py release
+
+    echo "Publishing to PyPI"
+    pip install twine
+    twine upload dist/* -u ${PYPI_USER_NAME} -p ${PYPI_PWD}
+}
+
 function main () {
     lint || return 1
     build || return 1
     run-test || return 1
+        
+    if [[ $TRAVIS_PYTHON_VERSION =~ ^3\.5+$ ]]; then
+        if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+            echo "Releasing tag $TRAVIS_TAG with Python $TRAVIS_PYTHON_VERSION"
+            release || return 1
+        fi
+    fi
+
 }
 
 main "$@" || exit 1
