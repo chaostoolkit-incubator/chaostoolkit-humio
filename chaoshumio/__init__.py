6fc2ce47ad7546912a2e68e9ac2aaebb68759352
Russ Miles <russ@russmiles.com>
2020-01-13 16:51:13 +0000

    Revved version for release
    Signed-off-by: Russ Miles <russ@russmiles.com>

--- chaoshumio/__init__.py
+++ chaoshumio/__init__.py
@@ -7,7 +7,7 @@ from logzero import logger
 import requests
 
 __all__ = ["__version__", "push_to_humio"]
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 
 def push_to_humio(event: EventPayload, secrets: Secrets,
